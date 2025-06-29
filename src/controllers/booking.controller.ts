b433fd3ec767e7a14f6d4978efecabcbc65ffdf8
LidTleJao <praphunpongtke@gmail.com>
2025-06-29 20:38:58 +0700

    install jwt login admin

--- src/controllers/booking.controller.ts
+++ src/controllers/booking.controller.ts
@@ -20,21 +20,7 @@
 //   }
 // };
 
-// export const getTodayBookings = async (req: Request, res: Response) => {
-//   const today = new Date();
-//   today.setHours(0, 0, 0, 0);
-//   const tomorrow = new Date(today);
-//   tomorrow.setDate(today.getDate() + 1);
 
-//   try {
-//     const bookings = await BookingModels.find({
-//       datetime: { $gte: today, $lt: tomorrow },
-//     });
-//     res.json(bookings);
-//   } catch (err) {
-//     res.status(500).json({ error: "ดึงข้อมูลล้มเหลว" });
-//   }
-// };
 
 import { Request, Response } from "express";
 import Booking from "../models/Booking.models";
@@ -63,4 +49,20 @@ export const createBooking = async (
     bookingDate,
   });
   res.status(201).json(booking);
+};
+
+export const getTodayBookings = async (req: Request, res: Response) => {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  const tomorrow = new Date(today);
+  tomorrow.setDate(today.getDate() + 1);
+
+  try {
+    const bookings = await Booking.find({
+      datetime: { $gte: today, $lt: tomorrow },
+    });
+    res.json(bookings);
+  } catch (err) {
+    res.status(500).json({ error: "ดึงข้อมูลล้มเหลว" });
+  }
 };
--- src/routes/Booking.routes.ts
+++ src/routes/Booking.routes.ts
@@ -1,17 +1,9 @@
-// import express from "express";
-// import { getAllBookings,createBooking, getTodayBookings } from "../controllers/booking.controller";
-
-// export const router = express.Router();
-
-// router.get("/queueAll", getAllBookings);
-// router.get("/today", getTodayBookings);
-// router.post("/", createBooking);
-
 import express from "express";
-import { createBooking, getBookings } from "../controllers/booking.controller";
+import { createBooking, getBookings, getTodayBookings } from "../controllers/booking.controller";
 
 export const router = express.Router();
 router.get("/", getBookings);
+router.get("/today", getTodayBookings);
 router.post("/", (req, res, next) => {
   Promise.resolve(createBooking(req, res, next)).catch(next);
 });
