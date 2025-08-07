75eacc9986a7113da081f6fc01c878c839bacaea
LidTleJao <praphunpongtke@gmail.com>
2025-08-08 00:44:43 +0700

    fix connecting fail

--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
 import http from "http";
-import { app } from "./index";
-import { connectDB } from "./config/db";
+// import { app } from "./index";
+// import { connectDB } from "./config/db";
+import { app } from "../back_end_smart_queue_nodejs/src/index";
+import { connectDB } from "../back_end_smart_queue_nodejs/src/config/db";
 
 const port = process.env.port || 5000;
 const server = http.createServer(app);
@@ -10,6 +12,6 @@ const server = http.createServer(app);
 // });
 connectDB().then(() => {
   app.listen(port, () => {
-    console.log(` Server running on http://localhost:${PORT}`);
+    console.log(` Server running on http://localhost:${port}`);
   });
 });
