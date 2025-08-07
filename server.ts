86f24e71e37ffda46d5fd550fb002d5ed5cbcd87
LidTleJao <praphunpongtke@gmail.com>
2025-08-08 00:51:07 +0700

    fix server.ys file

--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
 import http from "http";
 // import { app } from "./index";
 // import { connectDB } from "./config/db";
-import { app } from "../back_end_smart_queue_nodejs/src/index";
-import { connectDB } from "../back_end_smart_queue_nodejs/src/config/db";
+import { app } from "./src/index";
+import { connectDB } from "./src/config/db";
 
 const port = process.env.port || 5000;
 const server = http.createServer(app);
